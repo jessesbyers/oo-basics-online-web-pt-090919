c018b02d3ae4663b50b183d52d0f05b4a1706273
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-19 13:23:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,14 @@
 class Book
   def initialize(title)
     @title = title
+  end
 
+  def author(author)
+    @author = author
   end
 
   attr_accessor :title
+  attr_accessor :author
 
 end
 
@@ -12,6 +16,6 @@ end
 # @page_count = page_count
 # @genre = genre
 
-# attr_accessor :author
+
 # attr_accessor :page_count
 # attr_accessor :genre
